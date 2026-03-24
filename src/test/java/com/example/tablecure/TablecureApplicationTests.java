6b6a21396772da227c06abc3e8cd3388215d2fe0
Sachin Jangid <sachinreal2003@gmail.com>
2026-03-24 23:37:37 +0530

    fresh backend

---
+++ src/test/java/com/example/tablecure/TablecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tablecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
