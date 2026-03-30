57179de8b5cce0f2f413bc8af339c0709081e9c6
Sachin Jangid <sachinreal2003@gmail.com>
2026-03-30 15:21:35 +0530

    improved db query2

--- src/main/java/com/example/tablecure/entity/Product.java
+++ src/main/java/com/example/tablecure/entity/Product.java
@@ -17,6 +17,7 @@ public class Product {
     private Long id;
 
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String description;
     private BigDecimal price;
     private Integer stock;
